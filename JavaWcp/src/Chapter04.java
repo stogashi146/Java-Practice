e5edded9071101a19c33c036baf96a5221c8a6bb
HelloApp <stogashi146@gmail.com>
2021-10-05 17:33:00 +0900

    [Finish]Chapter04

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name;
+		name = "Shun";
+		System.out.println(name);
+		name = "アムロ上手くやれよ";
+		System.out.println(name);
+		final int LUCK_NUMBER = 555;
+		System.out.println(LUCK_NUMBER);
+
+	}
+
+}
