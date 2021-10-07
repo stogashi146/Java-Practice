dc39cb69307dc53a8d838933dd72809b3553e189
HelloApp <stogashi146@gmail.com>
2021-10-07 22:59:11 +0900

    [Finish] Chapter09

---
+++ JavaWcp/src/practice/Car.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Car {
+	private double fuelCost;
+	private double fuelAmount;
+
+	 // コンストラクタを作成
+
+    // moveメソッド
+    // ・"xx km 走ります"を出力
+    // ・残量を計算
+
+    // fuelAmountを取得するメソッドを作成
+}
