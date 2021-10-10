c855de6250501b110505005cf368b84aec3ed551
stogashi146 <stogashi146@gmail.com>
2021-10-10 16:21:55 +0900

    [Finish]Chapter9

--- JavaWcp/src/practice/Car.java
+++ JavaWcp/src/practice/Car.java
@@ -5,10 +5,22 @@ public class Car {
 	private double fuelAmount;
 
 	 // コンストラクタを作成
+	public Car(double fuelCost, double fuelAmount) {
+		this.fuelCost = fuelCost;
+		this.fuelAmount = fuelAmount;
+	}
 
     // moveメソッド
     // ・"xx km 走ります"を出力
     // ・残量を計算
-
+	public void move(int km) {
+		System.out.println(km + "km走ります");
+		this.fuelAmount -= (km / fuelCost);
+//		return distance + "km 走ります";
+	}
     // fuelAmountを取得するメソッドを作成
+	public double getFuelAmount() {
+//		return this.fuelCost = this.fuelCost - (distance / this.fuelCost);
+		return this.fuelAmount;
+	}
 }
